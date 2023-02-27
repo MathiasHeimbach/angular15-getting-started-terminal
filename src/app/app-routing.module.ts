a341551f695838836160d686b7d531a78d2ef9d2
MathiasHeimbach <60534997+MathiasHeimbach@users.noreply.github.com>
2023-02-27 14:30:06 +0100

    add routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+const routes: Routes = [{
+  path:'',
+  loadChildren: () => import("./cakes/cakes.module").then(_ => _.CakesModule)
+}]
+
+// create for.routing and export //
+ @NgModule({
+  imports: [ 
+    RouterModule.forRoot(routes)
+  ]
+})
+export class RoutingModule {}
